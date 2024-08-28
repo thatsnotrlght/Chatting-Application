471e8a36d3a585be89ef585f049e56f352953eb3
thatsnotrlght <jartiste23@gmail.com>
2024-08-27 23:55:39 -0400

    Added image to the JPanel

--- src/chatting/application/Server.java
+++ src/chatting/application/Server.java
@@ -16,9 +16,17 @@ public class Server extends JFrame{ //Server is now subclass of JFrame -> JFrame
 		//JPanel container
 		JPanel panel1 = new JPanel(); //Initialization of panel
 		panel1.setBackground(new Color(7, 94, 94));
-		panel1.setBounds(0, 100, 450, 70); //(x, y, width, height)
+		panel1.setBounds(0, 0, 450, 70); //(x, y, width, height)
+		panel1.setLayout(null);
 		add(panel1);
-
+		
+		ImageIcon iconFrame = new ImageIcon("src/icons/back_arrow.jpg"); //ImageIcon Class to hold relative path images
+		Image icon1 = iconFrame.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+		ImageIcon icon = new ImageIcon(icon1);
+		JLabel back_arrow = new JLabel(icon);
+		back_arrow.setBounds(5,20,25,25);
+		panel1.add(back_arrow); //Adds icon to panel
+		 
 		setVisible(true);
 	}
 	
